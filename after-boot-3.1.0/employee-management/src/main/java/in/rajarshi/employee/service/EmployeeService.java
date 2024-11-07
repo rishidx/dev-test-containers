2d85aebe539d50780db5f774ab5414b02a68cf41
Rajarshi Chakrabarty <rishidx@gmail.com>
2024-11-07 13:05:18 +0530

    Testcontainers (#2)
    * Adding files
    
    * added README.md file
    
    * Added new files

---
+++ after-boot-3.1.0/employee-management/src/main/java/in/rajarshi/employee/service/EmployeeService.java
@@ -0,0 +1,23 @@
+package in.rajarshi.employee.service;
+
+import in.rajarshi.employee.model.Employee;
+import in.rajarshi.employee.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EmployeeService {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+
+    public Employee createEmployee(Employee employee) {
+        return employeeRepository.save(employee);
+    }
+}
--- after-boot-3.1.0/employee-management/src/test/java/in/rajarshi/employee/repository/EmployeeRepositoryTest.java
+++ after-boot-3.1.0/employee-management/src/test/java/in/rajarshi/employee/repository/EmployeeRepositoryTest.java
@@ -1,36 +1,24 @@
 package in.rajarshi.employee.repository;
 
 import in.rajarshi.employee.model.Employee;
+import in.rajarshi.employee.testconfig.TestContainerConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
-import org.testcontainers.containers.MySQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
+import org.springframework.context.annotation.Import;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
-@Testcontainers
+@Import(TestContainerConfig.class)
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class EmployeeRepositoryTest {
 
-    @Container
-    static MySQLContainer<?> mysql = new MySQLContainer<>("mysql:9.1.0");
     @Autowired
     private EmployeeRepository employeeRepository;
-
-    @DynamicPropertySource
-    static void configureProperties(DynamicPropertyRegistry registry) {
-        registry.add("spring.datasource.url", mysql::getJdbcUrl);
-        registry.add("spring.datasource.username", mysql::getUsername);
-        registry.add("spring.datasource.password", mysql::getPassword);
-    }
 
     @Test
     public void testSaveEmployee() {
